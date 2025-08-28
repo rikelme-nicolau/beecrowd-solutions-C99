1e7b43b2883947f202b751a570edc81b788ac637
Rikelme Nicolau <contato.rikelmenicolau@gmail.com>
2025-08-28 13:15:42 -0300

    iniciante 1003

---
+++ iniciante/bee1003.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+    int a;
+    int b;
+    int soma;
+
+    scanf("%d", &a);
+    scanf("%d", &b);
+
+    soma = a + b;
+
+    printf("SOMA = %d\n", soma);
+
+    return 0;
+}
